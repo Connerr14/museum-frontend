dd512d11a7af426b8885661fb53a8689c1aaef60
Conner <ckrecosk@lakeheadu.ca>
2025-03-29 20:09:11 -0400

    Added styling to the nav and set up routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MuseumListComponent } from './components/museum-list/museum-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddMuseumComponent } from './components/add-museum/add-museum.component';
+import { EditMuseumComponent } from './components/edit-museum/edit-museum.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // The path for the museum lists page
+    {
+        path: 'museum', component: MuseumListComponent
+    },
+    {
+        path: "", component: HomeComponent
+    },
+    {
+        path: "addMuseum", component: AddMuseumComponent
+    },
+    {
+        path: "editMuseum", component: EditMuseumComponent
+    }
+];
---
+++ src/app/services/museum.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MuseumService {
+  serverUrl: string = environment.serverUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // A method to get all museums
+  getMuseums () {
+      // Make the api call
+      return this.http.get(`${this.serverUrl}/museums`);
+  }
+  
+  // A method to add a museum
+  addMuseum (museum: any) {
+    return this.http.post(`${this.serverUrl}/museums`, museum);
+  }
+
+  // A method to delete a museum
+  deleteMuseum (_id: string) {
+    return this.http.delete(`${this.serverUrl}/museums/${_id}`);
+  }
+
+  // A method to update a museum
+  updateMuseum (museum: any) {
+    return this.http.put(`${this.serverUrl}/museums/${museum._id}`, museum);
+  }
+}
